8e391b9d56af5cccb2a831e8dddd97e2414817f2
Thorres <simon.torres@usherbrooke.ca>
2020-01-16 21:15:10 -0500

    Initial nginx and angular

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -1,7 +1,31 @@
-var http = require('http');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url);
-  res.end();
-}).listen(8080);
+const bodyParser = require ('body-parser')
+const session = require('express-session')
+const path = require('path');
+
+app.use(session({ secret : 'zz', resave: true, saveUninitialized: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../frontend/dist/')));
+
+app.post('/api/auth', (req, res) => {
+    const { username, password } = req.body;
+
+    if( username === 'Thorres' && password === 'admin') {
+        req.session.auth = true;
+        req.session.user = username;
+        return res.json({ status: 'ok' });
+    }
+
+    return res.json({ status: 'error', data: `Username ${username} is invalid with this password` });
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(3000, ()  => {
+    console.log('Example');
+});
